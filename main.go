7dc7ad134d07c447a94fc160645b1535025afffa
chrzanekz <matix3010@gmail.com>
2023-01-03 15:20:18 +0100

    code changes

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome in out", conferenceName, "booking application!")
+	fmt.Println("Today, we have", conferenceTickets, "tickets and", remainingTickets, "tickets is still available.")
+	fmt.Println("Get your tickets here to attend")
 }
