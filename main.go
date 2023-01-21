f3b1494835f12f91e03f182688380a65d09bb4f6
chrzanekz <matix3010@gmail.com>
2023-01-21 12:08:32 +0100

    added %T function

--- main.go
+++ main.go
@@ -7,13 +7,18 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("confrenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+
 	fmt.Printf("Welcome in out %v booking application!\n", conferenceName)
 	fmt.Printf("Today, we have %v tickets and %v tickets is still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName
+	var userName string
+	var userTickets int
 	//ask user for his name
 
 	userName = "Mateusz"
-	fmt.Println(userName)
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+
 }
