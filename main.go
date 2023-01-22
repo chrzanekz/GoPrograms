da4868afe3159e0c082e286edde4a732ba4092df
chrzanekz <matix3010@gmail.com>
2023-01-22 13:32:00 +0100

    added for loop and slice method of array

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	bookings := []string{}
 
 	//some info for user
 	fmt.Printf("confrenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
@@ -15,35 +15,39 @@ func main() {
 	fmt.Printf("Today, we have %v tickets and %v tickets is still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	//declare user variables
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		//declare user variables
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	//ask user for his informations
-	fmt.Println("Enter first name:")
-	fmt.Scan(&firstName)
+		//ask user for his informations
+		fmt.Println("Enter first name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter email address:")
-	fmt.Scan(&email)
+		fmt.Println("Enter email address:")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter how many tickets do you want to buy:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter how many tickets do you want to buy:")
+		fmt.Scan(&userTickets)
 
-	//basic calculations
-	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+		//basic calculations
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Printf("the whole array is: %v \n", bookings)
-	fmt.Printf("the first value of array is: %v \n", bookings[0])
-	fmt.Printf("the type of array is: %T \n", bookings)
-	fmt.Printf("the length of array is: %v \n", len(bookings))
+		fmt.Printf("the whole slice is: %v \n", bookings)
+		fmt.Printf("the first value of slice is: %v \n", bookings[0])
+		fmt.Printf("the type of slice is: %T \n", bookings)
+		fmt.Printf("the length of slice is: %v \n", len(bookings))
 
-	//results on the screen
-	fmt.Printf("Thank You %v %v for buying %v tickets, you'll receive an email at %v with confirmation.\n", firstName, lastName, email, userTickets)
-	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
+		//results on the screen
+		fmt.Printf("Thank You %v %v for buying %v tickets, you'll receive an email at %v with confirmation.\n", firstName, lastName, email, userTickets)
+		fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
+
+		fmt.Printf("These our bookings: %v\n", bookings)
+	}
 }
