e28b00a47e5c1dd76a3ac08150bce97b648afba4
chrzanekz <matix3010@gmail.com>
2023-01-24 19:06:22 +0100

    added checking input variables from user

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Printf("Today, we have %v tickets and %v tickets is still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	for {
+	for remainingTickets > 0 && len(bookings) < 50 {
 		//declare user variables
 		var firstName string
 		var lastName string
@@ -38,27 +38,43 @@ func main() {
 		fmt.Println("Enter how many tickets do you want to buy:")
 		fmt.Scan(&userTickets)
 
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
 		//basic calculations
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName + " " + lastName)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
 
-		fmt.Printf("the whole slice is: %v \n", bookings)
-		fmt.Printf("the first value of slice is: %v \n", bookings[0])
-		fmt.Printf("the type of slice is: %T \n", bookings)
-		fmt.Printf("the length of slice is: %v \n", len(bookings))
+			fmt.Printf("the whole slice is: %v \n", bookings)
+			fmt.Printf("the first value of slice is: %v \n", bookings[0])
+			fmt.Printf("the type of slice is: %T \n", bookings)
+			fmt.Printf("the length of slice is: %v \n", len(bookings))
 
-		//results on the screen
-		fmt.Printf("Thank You %v %v for buying %v tickets, you'll receive an email at %v with confirmation.\n", firstName, lastName, email, userTickets)
-		fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
-		
-		//loop for creating first name slice
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			//results on the screen
+			fmt.Printf("Thank You %v %v for buying %v tickets, you'll receive an email at %v with confirmation.\n", firstName, lastName, email, userTickets)
+			fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
+			
+			//loop for creating first name slice
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			//print results
+			fmt.Printf("The first names clients of our bookings are: %v\n", firstNames)
+
+			
+			if remainingTickets == 0 {
+				//ending loop
+				fmt.Println("Our conference is booked out. Please come back next year.")
+				break
+			}
+		} else {
+			
+			fmt.Printf("Your input data is invalid, please try again"\n)
 		}
-
-		//print results
-		fmt.Printf("The first names clients of our bookings are: %v\n", firstNames)
 	}
 }
